a6faf199f2e8afb53409b94c029ff424856e2a81
robbertstapel <robbert.stapel@rabobank.nl>
2025-05-06 00:50:34 +0200

    Add assigment who am i

---
+++ 02-a-trip-to-objectville/045-who-am-i/WhoAmI.java
@@ -0,0 +1,44 @@
+class WhoAmI {
+    public static void main(String[] args) {
+        // I am compiled from a .java file
+        // Class
+
+        // My instance variable values can be different from my buddy's values
+        // Object (instance of an object)
+
+        // I behave like a template
+        // Class (blueprint) or a super class which is a template for a subclass
+
+        // I like to do stuff
+        // Method, Object - A method is used by the object to do stuff
+
+        // I can have many methods
+        // A class or object - a class can have a method an object can use it?
+
+        // I represtent state
+        // Instance variable(s)
+
+        // I have behaviours
+        // Class, Object - Uses method to devine behaviour (and state is devined by instance variables)
+
+        // I am located in objects
+        // State, Behaviour - Instance variables and methods
+
+        // I live on the heap
+        // Objects created on the heap and collected by the garbage collector once they are no longer referenced
+
+        // I am used to create object instances
+        // Class
+
+        // My state can change
+        // Instance variable(s), Object - Instance variables can change at runtime
+
+        // I declare methods
+        // Class
+
+        // I can change at runtime
+        // Object - Instance variables can change at runtime
+    }
+}
+
+// For now we don't care where state and behaviour are technically located
